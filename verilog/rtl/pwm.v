01c5da33d55eca09434a5157881a0ec89772b250
“emilgoh” <emil_goh@mymail.sutd.edu.sg>
2023-11-02 03:02:32 +0800

    RTL Integration

---
+++ verilog/rtl/pwm.v
@@ -0,0 +1,259 @@
+module pwm(
+    input wire clk,
+    input wire rst,
+    input wire en,  // Added enable input
+    input wire [7:0] duty_cycle,
+    output wire pwm1_out,
+    output wire pwm1_comp_out,
+    output wire pwm2_out,
+    output wire pwm2_comp_out,
+    output wire pwm3_out,
+    output wire pwm3_comp_out
+);
+
+    wire [7:0] corrected_duty_cycle;
+    wire cmp1_out, cmp2_out;
+    wire latch1_q, latch2_q;
+    wire [7:0] count1, count2, count3;
+
+    wire pwm1_raw, pwm2_raw, pwm3_raw;
+
+
+    // Dead Time Generation for each PWM signal
+    DeadTimeGenerator dtg1(
+        .clk(clk),
+        .pwm_in(PWM1.pwm_out),
+        .pwm_out(pwm1_out),  // Connect to ThreePhasePWM module's output ports
+        .comp_pwm_out(pwm1_comp_out)
+    );
+
+    DeadTimeGenerator dtg2(
+        .clk(clk),
+        .pwm_in(PWM2.pwm_out),
+        .pwm_out(pwm2_out),  // Connect to ThreePhasePWM module's output ports
+        .comp_pwm_out(pwm2_comp_out)
+    );
+
+    DeadTimeGenerator dtg3(
+        .clk(clk),
+        .pwm_in(PWM3.pwm_out),
+        .pwm_out(pwm3_out),  // Connect to ThreePhasePWM module's output ports
+        .comp_pwm_out(pwm3_comp_out)
+    );
+
+    DutyCycleCorrector dcc (
+        .duty_cycle_in(duty_cycle),
+        .duty_cycle_out(corrected_duty_cycle)
+    );
+
+    // Instantiate Single PWM Modules
+    PWM PWM1(
+        .clk(clk),
+        .rst(rst),
+        .en(en),  // Connected enable signal
+        .duty_cycle(corrected_duty_cycle),  // Updated to use corrected_duty_cycle
+        .pwm_out(pwm1_raw)
+    );
+    
+    PWM PWM2(
+        .clk(clk),
+        .rst(rst),
+        .en(latch1_q & en),  // Updated enable condition
+        .duty_cycle(corrected_duty_cycle),  // Updated to use corrected_duty_cycle
+        .pwm_out(pwm2_raw)
+    );
+    
+    PWM PWM3(
+        .clk(clk),
+        .rst(rst),
+        .en(latch2_q & en),  // Updated enable condition
+        .duty_cycle(corrected_duty_cycle),  // Updated to use corrected_duty_cycle        
+        .pwm_out(pwm3_raw)
+    );
+
+    // Connect counter outputs
+    assign count1 = PWM1.count;
+    assign count2 = PWM2.count;
+    assign count3 = PWM3.count;
+
+    // Comparator Modules
+    OneThirdComparator cmp1(
+        .count(count1),
+        .cmp_out(cmp1_out)
+    );
+    
+    OneThirdComparator cmp2(
+        .count(count2),
+        .cmp_out(cmp2_out)
+    );
+
+    // S-R Latch Modules
+    SR_latch latch1(
+        .en(en),
+        .rst(rst),
+        .S(cmp1_out),
+        .R(1'b0),
+        .Q(latch1_q)
+    );
+    
+    SR_latch latch2(
+        .en(en),
+        .rst(rst),
+        .S(cmp2_out),
+        .R(1'b0),
+        .Q(latch2_q)
+    );
+
+endmodule
+
+module DutyCycleCorrector(
+    input wire [7:0] duty_cycle_in,
+    output wire [7:0] duty_cycle_out
+);
+    assign duty_cycle_out = 8'hFF - duty_cycle_in;
+endmodule
+
+module OneThirdComparator(
+    input wire [7:0] count,
+    output wire cmp_out
+);
+    assign cmp_out = (count >= 8'd85) ? 1 : 0;  // 256/3 ≈ 85
+endmodule
+
+module SR_latch(
+    input en, rst, S, R, 
+    output reg Q
+    ); 
+    
+    always@(*)
+        begin 
+            if(rst) Q <= 1'b0;
+            else if(en) begin
+                case({S,R})
+                    2'b00 : Q <= Q;
+                    2'b01 : Q <= 1'b0;
+                    2'b10 : Q <= 1'b1;
+                    default : Q <= 1'bx;
+                endcase
+            end    
+        end
+endmodule
+
+
+// Single PWM module, Comparator, UpCounter, DFlipFlop modules remain unchanged
+module PWM(
+    input wire clk,
+    input wire rst,
+    input wire en,  // Added enable input
+    input wire [7:0] duty_cycle,
+    output wire pwm_out
+);
+    wire cmp_out;
+    wire [7:0] count;
+    
+    UpCounter u1(
+        .clk(clk),
+        .rst(rst),
+        .en(en),  // Connected enable signal
+        .count(count)
+    );
+    
+    Comparator c1(
+        .duty_cycle(duty_cycle),
+        .count(count),
+        .cmp_out(cmp_out)
+    );
+    
+    DFlipFlop d1(
+        .clk(clk),
+        .rst(rst),
+        .d(cmp_out),
+        .q(pwm_out)
+    );
+    
+endmodule
+
+module Comparator(
+    input wire [7:0] duty_cycle,
+    input wire [7:0] count,
+    output wire cmp_out
+);
+    assign cmp_out = (count > duty_cycle) ? 1 : 0;  // Changed from count < duty_cycle to count > duty_cycle
+endmodule
+
+module UpCounter(
+    input wire clk,
+    input wire rst,
+    input wire en,  // Added enable input
+    output wire [7:0] count
+);
+    reg [7:0] count_reg;
+    assign count = count_reg;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            count_reg <= 8'b0;
+        else if (en)  // Check enable signal
+            if (count_reg == 8'b11111111)
+                count_reg <= 8'b0;
+            else
+                count_reg <= count_reg + 1;
+    end
+endmodule
+
+module DFlipFlop(
+    input wire clk,
+    input wire rst,
+    input wire d,
+    output reg q
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            q <= 0;
+        else
+            q <= d;
+    end
+endmodule
+
+module DeadTimeGenerator(
+    input wire clk,
+    input wire pwm_in,
+    output wire pwm_out,
+    output wire comp_pwm_out
+);
+    wire dff1_out, dff2_out, dff3_out, dff4_out;
+
+    // Chain of four D Flip-Flops
+    DFlipFlop dff1(
+        .clk(clk),
+        .rst(1'b0),  // No reset
+        .d(pwm_in),
+        .q(dff1_out)
+    );
+
+    DFlipFlop dff2(
+        .clk(clk),
+        .rst(1'b0),
+        .d(dff1_out),
+        .q(dff2_out)
+    );
+
+    DFlipFlop dff3(
+        .clk(clk),
+        .rst(1'b0),
+        .d(dff2_out),
+        .q(dff3_out)
+    );
+
+    DFlipFlop dff4(
+        .clk(clk),
+        .rst(1'b0),
+        .d(dff3_out),
+        .q(dff4_out)
+    );
+
+    // Logic Gates
+    assign pwm_out = pwm_in & dff4_out;
+    assign comp_pwm_out = ~(pwm_in | dff4_out);
+
+endmodule
---
+++ verilog/rtl/user_proj_pwm.v
@@ -0,0 +1,98 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+/*
+ *-------------------------------------------------------------
+ *
+ * user_proj_example
+ *
+ * This is an example of a (trivially simple) user project,
+ * showing how the user project can connect to the logic
+ * analyzer, the wishbone bus, and the I/O pads.
+ *
+ * This project generates an integer count, which is output
+ * on the user area GPIO pads (digital output only).  The
+ * wishbone connection allows the project to be controlled
+ * (start and stop) from the management SoC program.
+ *
+ * See the testbenches in directory "mprj_counter" for the
+ * example programs that drive this user project.  The three
+ * testbenches are "io_ports", "la_test1", and "la_test2".
+ *
+ *-------------------------------------------------------------
+ */
+
+module user_proj_pwm #(
+    parameter BITS = 16
+)(
+`ifdef USE_POWER_PINS
+    inout vccd1,	// User area 1 1.8V supply
+    inout vssd1,	// User area 1 digital ground
+`endif
+
+    // Wishbone Slave ports (WB MI A)
+    input wb_clk_i,
+    input wb_rst_i,
+
+    // IOs
+    input  [14:0] io_in,
+    output [14:0] io_out,
+    output [14:0] io_oeb,
+);
+
+    // Input
+    wire en;
+    wire [7:0] duty_cycle;
+
+    //Output
+    wire pwm1_out, pwm1_comp_out;
+    wire pwm2_out, pwm2_comp_out;
+    wire pwm3_out, pwm3_comp_out;
+
+    pwm pwm(
+        .clk(wb_clk_i),
+        .rst(wb_rst_i),
+        .en(en),
+        .duty_cycle(duty_cycle),
+
+        .pwm1_out(pwm1_out),
+        .pwm1_comp_out(pwm1_comp_out),
+        .pwm2_out(pwm2_out),
+        .pwm2_comp_out(pwm2_comp_out),
+        .pwm3_out(pwm3_out),
+        .pwm3_comp_out(pwm3_comp_out)
+    );
+    
+    // Input
+    assign io_in[0] = en;
+    assign io_in[8:0] = duty_cycle;
+    
+    assign io_oeb[8:0] = 9'd1;
+
+    // Output
+    assign io_out[9] = pwm1_out;
+    assign io_out[10] = pwm1_comp_out;
+    assign io_out[11] = pwm2_out;
+    assign io_out[12] = pwm2_comp_out;
+    assign io_out[13] = pwm3_out;
+    assign io_out[14] = pwm4_comp_out;
+
+    assign io_oeb[14:9] = 6'd0;
+
+endmodule
+
+
+`default_nettype wire
